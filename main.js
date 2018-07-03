85cbdda87bffd9ae9c6156172f25f174e47106c4
takuminish <s15t253@stu.kagawa-u.ac.jp>
2018-07-04 01:11:24 +0900

    画面サイズの変更

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ app.on('window-all-closed', function() {
 // Electronの初期化完了後に実行
 app.on('ready', function() {
   // メイン画面の表示。ウィンドウの幅、高さを指定できる
-  mainWindow = new BrowserWindow({width: 200, height: 300});
+  mainWindow = new BrowserWindow({width: 600, height: 600});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // ウィンドウが閉じられたらアプリも終了
